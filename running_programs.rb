1f0723ff6cdde4b55ca2b7e74bce94d37908d44d
Carrie Snow <84377050+caerri@users.noreply.github.com>
2025-10-26 11:33:06 -0600

    arrays: joins, splits with adam driver

--- running_programs.rb
+++ running_programs.rb
@@ -34,23 +34,119 @@ print "#{2.even?} <-- 2.even?\n"
 print "#{4.odd?} <-- 4.odd?\n"
 print "#{3.even?} <-- 3.even?\n"
 print "#{3.odd?} <-- 3.odd?\n"
+# other update value types += -= /= *= are similar
 print "--------------------- array stuff ---------------------\n"
 print " will do prints later. "
-    # in irb, this is an example for assigning a variable
-    # to the previous array entered in irb:
-    # irb(main):005> ["this", "is", "an", "array"]
-    # => ["this", "is", "an", "array"]
-    # irb(main):006> arr1 = _
-    # => ["this", "is", "an", "array"]
-    # irb(main):007> arr1
-    # => ["this", "is", "an", "array"]
-    # irb(main):008>
+# in irb, this is an example for assigning a variable
+# to the previous array entered in irb:
+# irb(main):005> ["this", "is", "an", "array"]
+# => ["this", "is", "an", "array"]
+# irb(main):006> arr1 = _
+# => ["this", "is", "an", "array"]
+# irb(main):007> arr1
+# => ["this", "is", "an", "array"]
+# irb(main):008>
 
-    # ------ you can find the index in reverse by doing ... -----
-    # irb(main):011> arr1[0]
-    # => "this"
-    # irb(main):012> arr1[-1]
-    # => "array"
+# ------ you can find the index in reverse by doing ... -----
+# irb(main):011> arr1[0]
+# => "this"
+# irb(main):012> arr1[-1]
+# => "array"
+
+# ----- what happens if the index does not exist => nil -----
+# rb(main):006> arr = _
+# => [1, 2, 3, 4, 5]
+# irb(main):007> arr[-1]
+# => 5
+# irb(main):008> arr[-4]
+# => 2
+# irb(main):009> arr[-5]
+# => 1
+# irb(main):010> arr[-6]
+# => nil
+
+# --- updating indexes within the array are as expected ---
+# irb(main):005> arr = [1, 2, 3, 4, 5]
+# => [1, 2, 3, 4, 5]
+# irb(main):006> arr[0] = 25
+# => 25
+# irb(main):007> arr
+# => [25, 2, 3, 4, 5]
+# irb(main):008> arr[-1] = arr[-1] * 10
+# => 50
+# irb(main):009> arr
+# => [25, 2, 3, 4, 50]
+# irb(main):010> arr[1] += 6
+# => 8
+# irb(main):011> arr
+# => [25, 8, 3, 4, 50]
+# irb(main):012>
+
+
+# --- shovel operator << is like a push to end of array
+# irb(main):011> arr
+# => [25, 8, 3, 4, 50]
+# irb(main):012> arr << 100
+# => [25, 8, 3, 4, 50, 100]
+
+# or use push method
+# => [25, 8, 3, 4, 50, 100]
+# irb(main):013> arr.push(200)
+# => [25, 8, 3, 4, 50, 100, 200]
+# irb(main):014> arr.push("Hello")
+# => [25, 8, 3, 4, 50, 100, 200, "Hello"]
+
+# remove last element, pop is used as expected:
+# => [25, 8, 3, 4, 50, 100, 200, "Hello"]
+# irb(main):015> arr.pop
+# => "Hello"
+# irb(main):016> arr
+# => [25, 8, 3, 4, 50, 100, 200]
+
+# unshift adds a prepended value to beginning/first of array
+# irb(main):016> arr
+# => [25, 8, 3, 4, 50, 100, 200]
+# irb(main):017> arr.unshift("World")
+# => ["World", 25, 8, 3, 4, 50, 100, 200]
+# irb(main):018> arr.shift
+# => "World"
+# irb(main):019> arr
+# => [25, 8, 3, 4, 50, 100, 200]
+
+# there is also a prepend method that makes more sense in english
+# irb(main):019> arr
+# => [25, 8, 3, 4, 50, 100, 200]
+# irb(main):020> arr.prepend("Hello, world")
+# => ["Hello, world", 25, 8, 3, 4, 50, 100, 200]
+
+# arr.length works as expected. size method: arr.size does the same
+# irb(main):023> arr
+# => [25, 8, 3, 4, 50, 100, 200]
+# irb(main):024> arr.length
+# => 7
+# irb(main):025> arr.size
+# => 7
+
+# --------- arrays and joining: ------
+# irb(main):026> framework = ["Ruby", "on", "Rails"]
+# => ["Ruby", "on", "Rails"]
+# irb(main):027> framework.join
+# => "RubyonRails"
+# irb(main):028>
+
+# --------- what if we want spaces?? !! ?? --------
+# => ["Ruby", "on", "Rails"]
+# irb(main):029> framework.join(" ")
+# => "Ruby on Rails"
+
+# ---- Now we split! ---- by default split splits by space #
+# irb(main):030> "Ruby on Rails".split
+# => ["Ruby", "on", "Rails"]
+
+# --- Splitting by "" gives you each character indexed into the array
+# irb(main):031> "Ruby on Rails".split("")
+# => ["R", "u", "b", "y", " ", "o", "n", " ", "R", "a", "i", "l", "s"]
+
 print "--------------------- enumerating --------------------- \n"
 print "#{arr=[]
 3.times {|i| arr.push i}
@@ -59,4 +155,8 @@ arr} <--   arr=[]                   # to open an empty array
                 arr                      # then prints the new array
                                          # note to self: is |i| indicating .abs?" #TODO: answer question
 print "\n----------------------- strings! ----------------------\n"
-
+# ----- Strings and array stuff -----
+# irb(main):033> arr
+# => ["R", "u", "b", "y", " ", "o", "n", " ", "R", "a", "i", "l", "s"]
+# irb(main):034> arr.size
+# => 13
