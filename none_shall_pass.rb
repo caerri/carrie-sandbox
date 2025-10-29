086c557c4771dd8dd1aa5ef0c006e22ce7448921
Carrie Snow <84377050+caerri@users.noreply.github.com>
2025-10-28 22:05:48 -0600

    refactoring pw app to methods

--- none_shall_pass.rb
+++ none_shall_pass.rb
@@ -14,77 +14,104 @@ PASSWORD_VAULT = {aws:
         credential1: "stuff" }
 }
 
-#### Detour into the world of constants ####
-# Here is an example of what the warning looks like.
-# irb(main):003> EMAIL
-# => "carrie@example.com"
-# irb(main):004> EMAIL = "something_else"
-# (irb):4: warning: already initialized constant EMAIL
-# (irb):1: warning: previous definition of EMAIL was here
-# => "something_else"
-# irb(main):005> EMAIL
-# => "something_else"
-# but also with the example Password = "asdf" changing it
-# resulted in the same warning.
-#### End Detour ####
-
-puts 'Welcome to None Shall Pass - A Password Manager'
-puts 'Please sign in to continue'
-print 'Enter email: '
-user_email = gets.chomp # This is to remove the newline "\n"
-puts "You entered: #{user_email}." # character if printing instead of puts
-
-if user_email == EMAIL
-    print "Enter Password: "
-    user_password = gets.chomp
-else
-    puts "#{user_email} is an Invalid email."
-    exit
+def welcome_message
+    puts 'Welcome to None Shall Pass - A Password Manager'
 end
 
-unless user_password != PASSWORD
+def prompt_user_for_email
+    puts 'Please sign in to continue'
+    print 'Enter email: '
+    puts "You entered: #{user_email}." # character if printing instead of puts
+end
+
+def verify_user_email(user_email)
+    if user_email != EMAIL
+        print "Invalid email"
+        exit
+    end
+end
+
+def prompt prompt_user_for_email
+    print "Enter Password"
+    gets.chomp
+end
+
+def verify_user_password(user_password)
+    if user_password != PASSWORD
+        puts "Invalid password"
+        exit
+    end
+end
+
+def greet user(user_email)
     puts "Hello #{user_email}"
     puts "What would you like to do?"
+end
+
+def menu_options)
     puts "1. Add new service credentials?"
     puts "2. Retrieve an existing service's credentials"
     puts "3. Exit"
-    print "Please type a selection: "
-    user_selection = gets.chomp
-else
-    puts "Invalid Password."
-    exit
 end
 
-case user_selection # like switch"
-when "1"
-    print "This will create new service credentials."
+def get_user_menu_selection
+    gets.chomp
+end
+
+def handle_user_selection(user_selection)
+    case user_selection # like switch"
+    when "1"
+        new_service = set_new_service_name
+        set_username_for(new_service)
+        set_password_for(new_service)
+    when "2"
+        requested_service_name = retrieve_service_name
+        credentials = retrieve_service_credentials_for(requested_service_name)
+        display_service_credentials(requested_service_name, credentials)
+    else
+        exit_program
+    end
+end
+
+def set_new_service_name
     print "What is the name of the new service?: "
     new_service = gets.chomp
-    PASSWORD_VAULT[new_service] = {}
+    PASSWORD_VAULT[new_service.to_sym] = {}
+    new_service
+end
 
+def set_username_for(service)
     print "Please enter the username for the #{PASSWORD_VAULT} service: "
     new_service_username = gets.chomp
-    PASSWORD_VAULT[new_service]["username"] = new_service_username
+    PASSWORD_VAULT[service.to_sym][:username] = new_service_username
+end
 
+def set_password_for(service)
     print "Please enter the password for the #{PASSWORD_VAULT} service"
     new_service_password = gets.chomp
-    PASSWORD_VAULT[new_service]["password"] = new_service_password
+    PASSWORD_VAULT[service.to_sym][:password] = new_service_password
+end
 
-    p PASSWORD_VAULT
-when "2"
-    puts "This will retrieve existing credentials."
+def retrieve_service_name
     puts "Please enter the name of the service you'd like to access credentials for: "
-    requested_service_name = gets.chomp
-    credentials = PASSWORD_VAULT[requested_service_name.to_sym]
-    puts "The credentials for #{requested_service_name} are: "
+    gets.chomp
+end
+
+def retrieve_service_credentials_for_(service_name)
+    PASSWORD_VAULT[service_name.to_sym]
+end
+
+def display_service_credentials(service_name, credentials)
+    puts "The credentials for #{service_name} are: "
     credentials.each do |key, val|
         puts "#{key}: #{val}"
     end
-else
-    puts "Exiting program."
-    exit
 end
 
+def exit_program
+    puts "Exiting program now."
+    exit
+end
 # Notes from end of video: Ternary operator
 # irb(main):047> number.odd? ? "odd" : "even"
 # => "odd"
